f63fe7fbadc735b3b724fd1319a01fa267c3255c
Vallamdasu Abhinay Srivastav <abhinaysrivasthav@gmail.com>
2023-12-28 18:30:07 +0530

    Committing Multiplication_Of_Two_MultiDimensional_Arrays_Example File

--- Core_Java_MultiDimensional_Arrays_Examples/src/com/abhinay/multidimensional/arrays/Multiplication_Of_Two_MultiDimensional_Arrays_Example.java
+++ Core_Java_MultiDimensional_Arrays_Examples/src/com/abhinay/multidimensional/arrays/Multiplication_Of_Two_MultiDimensional_Arrays_Example.java
@@ -1,6 +1,6 @@
 package com.abhinay.multidimensional.arrays;
 
-public class Substraction_Of_Two_MultiDimensional_Arrays_Example {
+public class Multiplication_Of_Two_MultiDimensional_Arrays_Example {
 
 	public static void main(String[] args) {
 		int[][] multi_dim_array_a = { { 10, 20, 30 }, { 40, 50, 60 }, { 70, 80, 90 }, { 100, 110, 120 } };
@@ -8,7 +8,7 @@ public class Substraction_Of_Two_MultiDimensional_Arrays_Example {
 		int number_of_rows = multi_dim_array_a.length; // 4 represents number of rows in multi_dim_array_a
 		int number_of_columns = multi_dim_array_a[0].length; // 3 represents number of columns for row-0 in
 																// multi_dim_array_a
-		int[][] arrays_difference = new int[number_of_rows][number_of_columns];
+		int[][] arrays_multiply = new int[number_of_rows][number_of_columns];
 
 		// displaying the multi_dim_array_a in Matrix format
 		System.out.println("\nData Representation of Matrix A:");
@@ -16,16 +16,17 @@ public class Substraction_Of_Two_MultiDimensional_Arrays_Example {
 		// displaying the multi_dim_array_b in Matrix format
 		System.out.println("\nData Representation of Matrix B:");
 		printArrayInMatrixFormat(multi_dim_array_b);
-		// traversing through the two multidimensional arrays and calculating the difference of
+		// traversing through the two multidimensional arrays and calculating the
+		// multiplication of
 		// their elements
 		// and storing in new multidimensional array
-		System.out.println("\nData Representation in Matrix Substraction:");
+		System.out.println("\nData Representation in Matrix Multiplication:");
 		for (int row = 0; row < number_of_rows; row++) {
 			for (int column = 0; column < number_of_columns; column++) {
-				arrays_difference[row][column] = multi_dim_array_a[row][column] - multi_dim_array_b[row][column];
+				arrays_multiply[row][column] = multi_dim_array_a[row][column] * multi_dim_array_b[row][column];
 			}
 		}
-		printArrayInMatrixFormat(arrays_difference);
+		printArrayInMatrixFormat(arrays_multiply);
 	}
 
 	public static void printArrayInMatrixFormat(int[][] result) {
