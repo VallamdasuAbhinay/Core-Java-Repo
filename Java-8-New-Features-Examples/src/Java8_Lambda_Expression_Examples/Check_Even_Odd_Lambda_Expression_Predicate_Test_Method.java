ff706a0f8bd01100febcf78f7687cfae0565a982
Vallamdasu Abhinay Srivastav <abhinaysrivasthav@gmail.com>
2024-01-02 17:31:30 +0530

    Committing Check_Even_Odd_Lambda_Expression_Predicate_Test_Method File

---
+++ Java-8-New-Features-Examples/src/Java8_Lambda_Expression_Examples/Check_Even_Odd_Lambda_Expression_Predicate_Test_Method.java
@@ -0,0 +1,36 @@
+package Java8_Lambda_Expression_Examples;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Check_Even_Odd_Lambda_Expression_Predicate_Test_Method {
+
+	public static void main(String[] args) {
+		System.out.println("Starting Check_Even_Odd_Lambda_Expression_Predicate_Test_Method....");
+		Predicate<Integer> evenPredicate = (int_num) -> (int_num % 2 == 0);
+		Predicate<Integer> oddPredicate = (int_num) -> (int_num % 2 != 0);
+		List<Integer> integersList = Arrays.asList(100, 20, 23, 46, 50, 90, 11, 33);
+		System.out.println("Even Numbers from the Array List " + integersList + " are ");
+		integersList.stream().filter(evenPredicate).forEach(System.out::println);
+		System.out.println("Odd Numbers from the Array List " + integersList + " are ");
+		integersList.stream().filter(oddPredicate).forEach(System.out::println);
+	}
+
+}
+/*
+ * Explanation:
+ * 
+ * In the above exercise, we have defined Two Predicate functional interfaces
+ * evenPredicate --> This predicate is used to check if a given number is even
+ * evenPredicate --> This predicate is used to check if a given number is odd
+ * 
+ * Predicate is a functional interface representing a single argument function
+ * that returns a boolean value.
+ * 
+ * Predicates in Java are implemented with interfaces. Predicate<T> is a generic
+ * functional interface representing a single argument function that returns a
+ * boolean value. It is located in the java.util.function package. It contains a
+ * Test (T t) method that evaluates the predicate on the given argument.
+ * 
+ */
