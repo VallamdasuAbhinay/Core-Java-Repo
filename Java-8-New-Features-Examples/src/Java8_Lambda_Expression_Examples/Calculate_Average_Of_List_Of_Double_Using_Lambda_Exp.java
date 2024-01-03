a286f59b0c6049cc5f2a75e7840c11c673fadb61
Vallamdasu Abhinay Srivastav <abhinaysrivasthav@gmail.com>
2024-01-03 18:09:30 +0530

    Committing Calculate_Average_Of_List_Of_Double_Using_Lambda_Exp File

---
+++ Java-8-New-Features-Examples/src/Java8_Lambda_Expression_Examples/Calculate_Average_Of_List_Of_Double_Using_Lambda_Exp.java
@@ -0,0 +1,28 @@
+package Java8_Lambda_Expression_Examples;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Calculate_Average_Of_List_Of_Double_Using_Lambda_Exp {
+
+	public static void main(String[] args) {
+		System.out.println("Starting Calculate_Average_Of_List_Of_Double_Using_Lambda_Exp....");
+		List<Double> marksList = Arrays.asList(89.2, 39.5, 59.4, 99.1, 88.4, 90.4);
+		System.out.println("Initial list of marks are....\n"+marksList);
+		Double average = marksList.stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
+		System.out.println("The average of student marks is "+average);
+	}
+
+}
+/*
+ * Explanation:
+ * 
+ * To calculate the average of the list, we use the stream() method on the
+ * marksList to convert it into a stream. Then, we use the mapToDouble() method
+ * to convert each Double object to its corresponding primitive double value.
+ * This step is necessary because the average() method operates on primitive
+ * double values.
+ * 
+ * Next, we call the average() method to calculate the average of the stream. If
+ * the stream is empty, we use orElse(0.0) to provide a default value of 0.0.
+ */
