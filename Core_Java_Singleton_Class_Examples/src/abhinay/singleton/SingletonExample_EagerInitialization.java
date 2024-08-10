9474c7ab7c6f8c8415fa5a2379ca54f27e4691a0
Vallamdasu Abhinay Srivastav <abhinaysrivasthav@gmail.com>
2024-08-10 17:52:21 +0530

    Updated Java Version From 1.8 To 17

--- Core_Java_Singleton_Class_Examples/src/abhinay/singleton/SingletonExample_EagerInitialization.java
+++ Core_Java_Singleton_Class_Examples/src/abhinay/singleton/SingletonExample_EagerInitialization.java
@@ -28,7 +28,7 @@ public class SingletonExample_EagerInitialization {
 	 * class.
 	 * 
 	 * In eager initialization, the instance of the singleton class is created at
-	 * the time of class loading. The drawback to eager initialization is that the
+	 * the time of class loading. The drawback of eager initialization is that the
 	 * method is created even though the client application might not be using it
 	 */
 
