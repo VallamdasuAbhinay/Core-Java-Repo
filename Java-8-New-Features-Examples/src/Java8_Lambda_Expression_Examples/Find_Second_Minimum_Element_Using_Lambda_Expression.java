35944d49b537818447feeb8573143f1e4fe8f3c7
Vallamdasu Abhinay Srivastav <abhinaysrivasthav@gmail.com>
2024-08-11 19:01:21 +0530

    Updated Find_Second_Minimum_Element_Using_Lambda_Expression

--- Java-8-New-Features-Examples/src/Java8_Lambda_Expression_Examples/Find_Second_Minimum_Element_Using_Lambda_Expression.java
+++ Java-8-New-Features-Examples/src/Java8_Lambda_Expression_Examples/Find_Second_Minimum_Element_Using_Lambda_Expression.java
@@ -13,6 +13,17 @@ public class Find_Second_Minimum_Element_Using_Lambda_Expression {
 		// by default sorted() will sort the elements in the ascending order
 		Integer second_min = Arrays.asList(marksArray).stream().distinct().sorted().skip(1).findFirst().orElse(null);
 		System.out.println("Second Minimum Element is " + second_min);
+
+		Integer[] scoresArray = { 100, 30, 100, 40, 190, 48, 400, 200, 100 };
+		System.out.println("Initial elements in the array are....");
+		Arrays.stream(scoresArray).forEach(System.out::println);
+		System.out.println("");
+		System.out.println("Initial elements in the array (sorted) are....");
+		Arrays.stream(scoresArray).sorted().forEach(System.out::println);
+		// by default sorted() will sort the elements in the ascending order
+		Integer second_min_in_scores_array = Arrays.stream(scoresArray).distinct().sorted().skip(1).findFirst()
+				.orElse(null);
+		System.out.println("Second Minimum Element is " + second_min_in_scores_array);
 	}
 
 }
