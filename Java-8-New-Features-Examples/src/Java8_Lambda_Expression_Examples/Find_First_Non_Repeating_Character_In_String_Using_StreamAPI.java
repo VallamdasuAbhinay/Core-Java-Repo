7994436dca6c66ec735670614a899eca5101b6f6
Vallamdasu Abhinay Srivastav <abhinaysrivasthav@gmail.com>
2024-08-11 21:06:59 +0530

    Committing Find_First_Non_Repeating_Character_In_String_Using_StreamAPI

---
+++ Java-8-New-Features-Examples/src/Java8_Lambda_Expression_Examples/Find_First_Non_Repeating_Character_In_String_Using_StreamAPI.java
@@ -0,0 +1,19 @@
+package Java8_Lambda_Expression_Examples;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Find_First_Non_Repeating_Character_In_String_Using_StreamAPI {
+
+	public static void main(String[] args) {
+		String sentence = "LenovoLaptop";
+		
+		String firstNonRepeatingChar = Arrays.stream(sentence.split(""))
+				.collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+		.entrySet().stream().filter(f -> f.getValue()==1).findFirst().get().getKey();
+		System.out.println("First Non-repeating character is "+firstNonRepeatingChar);
+	}
+
+}
