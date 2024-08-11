707cd1f4f9cc4439c0015085ace35441e25a24f1
Vallamdasu Abhinay Srivastav <abhinaysrivasthav@gmail.com>
2024-08-11 20:00:40 +0530

    Updated Print_Unique_Elements_In_List_Using_Stream_API

--- Java-8-New-Features-Examples/src/Java8_Lambda_Expression_Examples/Print_Unique_Elements_In_List_Using_Stream_API.java
+++ Java-8-New-Features-Examples/src/Java8_Lambda_Expression_Examples/Print_Unique_Elements_In_List_Using_Stream_API.java
@@ -1,6 +1,8 @@
 package Java8_Lambda_Expression_Examples;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -12,10 +14,16 @@ public class Print_Unique_Elements_In_List_Using_Stream_API {
 		System.out.println("Initial elements in the array are....");
 		Arrays.asList(marksArray).stream().forEach(System.out::println);
 		System.out.println("Unique elements (appeared only once) in the list are....");
-
+		// printing the unique elements in the list
 		Arrays.stream(marksArray).collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet()
 				.stream().filter(f -> f.getValue() == 1).forEach(p -> System.out.println(p.getKey()));
-		
+
+		// collecting all the unique elements into a list and printing them
+		List<Integer> uniqueElementsList = Arrays.stream(marksArray)
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream()
+				.filter(f -> f.getValue() == 1).map(Map.Entry::getKey).collect(Collectors.toList());
+		System.out.println(uniqueElementsList);
+
 	}
 
 }
