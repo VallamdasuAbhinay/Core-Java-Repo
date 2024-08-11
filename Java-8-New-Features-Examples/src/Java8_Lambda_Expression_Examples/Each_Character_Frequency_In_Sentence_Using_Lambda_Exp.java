35a3c7b7ebd18e41b08aa727c0b813c84cf605f6
Vallamdasu Abhinay Srivastav <abhinaysrivasthav@gmail.com>
2024-08-11 19:01:00 +0530

    Updated Each_Character_Frequency_In_Sentence_Using_Lambda_Exp

--- Java-8-New-Features-Examples/src/Java8_Lambda_Expression_Examples/Each_Character_Frequency_In_Sentence_Using_Lambda_Exp.java
+++ Java-8-New-Features-Examples/src/Java8_Lambda_Expression_Examples/Each_Character_Frequency_In_Sentence_Using_Lambda_Exp.java
@@ -1,5 +1,6 @@
 package Java8_Lambda_Expression_Examples;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
@@ -16,6 +17,14 @@ public class Each_Character_Frequency_In_Sentence_Using_Lambda_Exp {
 		System.out.println(frequencyMap);
 		//displaying the frequency of each character
 		frequencyMap.forEach((k,v) -> System.out.print("The Character " + k + " appeared " + v + " times\n"));
+		System.out.println("---------------");
+		Map<String, Long> frequencyMap2 = new HashMap<String, Long>();
+		String sentence2 = "dog name is juli juli. That dog is sitting in the sofa";
+		frequencyMap2 = Arrays.stream(sentence2.split("")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		System.out.println(frequencyMap2);
+		//displaying the frequency of each character
+		frequencyMap2.forEach((k,v) -> System.out.print("The Character " + k + " appeared " + v + " times\n"));
+		
 	}
 
 }
