1f402076ffe22baa2c6248433819ffa079c5b38f
Vallamdasu Abhinay Srivastav <abhinaysrivasthav@gmail.com>
2024-09-08 20:07:43 +0530

    Committing Calulate_Male_Female_Students_Count_Using_StreamAPI

---
+++ Java-8-New-Features-Examples/src/Java8_Lambda_Expression_Examples/Calulate_Male_Female_Students_Count_Using_StreamAPI.java
@@ -0,0 +1,26 @@
+package Java8_Lambda_Expression_Examples;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Calulate_Male_Female_Students_Count_Using_StreamAPI {
+
+	public static void main(String[] args) {
+
+		List<StudentClass> studentsList = Arrays.asList(
+				new StudentClass("Abhinay", "Vastav", "Section 4", "Hyderabad", "Male"),
+				new StudentClass("Archana", "Vallamdasu", "Section 2", "Hyderabad", "Female"),
+				new StudentClass("Urmila", "Vallamdasu", "Section 1", "Hyderabad", "Female"),
+				new StudentClass("Amarendar", "Vallamdasu", "Section 9", "Hyderabad", "Male"),
+				new StudentClass("Srithik", "Vallamdasu", "Section 10", "Hyderabad", "Male"));
+
+		Map<String, Long> resultMap = studentsList.stream()
+				.collect(Collectors.groupingBy(StudentClass::getGender, Collectors.counting()));
+		
+		System.out.println(resultMap);
+
+	}
+
+}
