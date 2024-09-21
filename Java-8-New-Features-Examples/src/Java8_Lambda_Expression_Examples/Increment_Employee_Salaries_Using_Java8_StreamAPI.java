6e74674f7f090c127c1dfae5210a98a812ae8962
Vallamdasu Abhinay Srivastav <abhinaysrivasthav@gmail.com>
2024-09-21 13:08:37 +0530

    Updated Increment_Employee_Salaries_Using_Java8_StreamAPI

--- Java-8-New-Features-Examples/src/Java8_Lambda_Expression_Examples/Increment_Employee_Salaries_Using_Java8_StreamAPI.java
+++ Java-8-New-Features-Examples/src/Java8_Lambda_Expression_Examples/Increment_Employee_Salaries_Using_Java8_StreamAPI.java
@@ -3,7 +3,7 @@ package Java8_Lambda_Expression_Examples;
 import java.util.Arrays;
 import java.util.List;
 
-public class Add_Increment_To_Employees_Salaries_And_Pint_Them_Using_StreamAPI {
+public class Increment_Employee_Salaries_Using_Java8_StreamAPI {
 
 	public static void main(String[] args) {
 		List<EmployeeClass> employeesList = Arrays.asList(
@@ -14,13 +14,13 @@ public class Add_Increment_To_Employees_Salaries_And_Pint_Them_Using_StreamAPI {
 				new EmployeeClass("Amarendar", "Vallamdasu", "Banking", "Hyderabad", 190000));
 		double incrementAmount = 10000d;
 		System.out.println("Employees Salaries Before Adding Increment..........");
-		employeesList.stream().forEach(emp -> System.out.println(emp));
+		employeesList.stream().forEach(System.out::println);
 		System.out.println("------------------------");
 		System.out.println("Employees Salaries After Adding Increment..........");
 
 		// use the below statement that will print only incremented salaries and
 		// original employee salary data will not get modified
-		employeesList.stream().map(emp -> emp.getSalary() + incrementAmount).forEach(f -> System.out.println(f));
+		employeesList.stream().map(emp -> emp.getSalary() + incrementAmount).forEach(System.out::println);
 		
 		//use below statement to print entire employee object and this will modify the original employee salary data
 		employeesList.stream().map(emp -> {
