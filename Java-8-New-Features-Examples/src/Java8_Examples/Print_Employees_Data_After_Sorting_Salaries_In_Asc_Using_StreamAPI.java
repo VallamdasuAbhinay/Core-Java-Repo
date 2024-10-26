2742d5934826ab7621421a00727bddcc9932029c
Vallamdasu Abhinay Srivastav <abhinaysrivasthav@gmail.com>
2024-10-26 14:33:11 +0530

    Changes made for Employee Salary Sort Logic

--- Java-8-New-Features-Examples/src/Java8_Examples/Print_Employees_Data_After_Sorting_Salaries_In_Asc_Using_StreamAPI.java
+++ Java-8-New-Features-Examples/src/Java8_Examples/Print_Employees_Data_After_Sorting_Salaries_In_Asc_Using_StreamAPI.java
@@ -9,13 +9,13 @@ public class Print_Employees_Data_After_Sorting_Salaries_In_Asc_Using_StreamAPI 
 	public static void main(String[] args) {
 		List<EmployeeClass> employeesList = Arrays.asList(
 				new EmployeeClass("Abhinay", "Vallamdasu", "Banking", "Hyderabad", 160000),
-				new EmployeeClass("Archana", "Vallamdasu", "IT", "Hyderabad", 190000),
+				new EmployeeClass("Archana", "Vallamdasu", "IT", "Hyderabad", 190001),
 				new EmployeeClass("Srithik", "Vallamdasu", "CSE", "Hyderabad", 120000),
 				new EmployeeClass("Urmila", "Vallamdasu", "Mech", "Hyderabad", 100000),
-				new EmployeeClass("Amarendar", "Vallamdasu", "Banking", "Hyderabad", 190000));
+				new EmployeeClass("Amarendar", "Vallamdasu", "Banking", "Hyderabad", 190000.5));
 		System.out.println("Printing the Employees Data after sorting them based on salaries in ascending order");
-		employeesList.stream().sorted(Comparator.comparing(EmployeeClass::getSalary))
-				.forEach(emp -> System.out.println(emp));
+		employeesList.stream().sorted(Comparator.comparingDouble(EmployeeClass::getSalary))
+				.forEach(System.out::println);
 	}
 
 }
