aefb0edc6afef49d94fb74fa2ad389bb2824a4c8
Vallamdasu Abhinay Srivastav <abhinaysrivasthav@gmail.com>
2025-08-17 20:48:17 +0530

    Updated Integer_To_Binary_Representation_Using_Lambda_Exp File

--- Java-8-New-Features-Examples/src/java8_programs/Integer_To_Binary_Representation_Using_Lambda_Exp.java
+++ Java-8-New-Features-Examples/src/java8_programs/Integer_To_Binary_Representation_Using_Lambda_Exp.java
@@ -17,8 +17,8 @@ public class Integer_To_Binary_Representation_Using_Lambda_Exp {
 
 /*
  * Explanation: This program uses the Integer.toBinaryString(n) method to
- * convert an integer *n to its respective binary representation. The lambda
- * expression allows you I to encapsulate this conversion logic and use it as a
+ * convert an integer "n" to its respective binary representation. The lambda
+ * expression allows you to encapsulate this conversion logic and use it as a
  * function. Function is the functional interface which accepts one argument and
  * produces the result. apply(T) is the functional method available inside this
  * Function.
