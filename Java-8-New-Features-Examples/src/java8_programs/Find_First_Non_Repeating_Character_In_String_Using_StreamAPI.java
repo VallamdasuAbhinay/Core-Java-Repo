dfdc93ff0eadbe689c0d19c2e2c5e58f57368207
Vallamdasu Abhinay Srivastav <abhinaysrivasthav@gmail.com>
2025-08-19 19:26:38 +0530

    Updated First_Non_Repeating_Character_In_String_Using_Java8 File

--- Java-8-New-Features-Examples/src/java8_programs/Find_First_Non_Repeating_Character_In_String_Using_StreamAPI.java
+++ Java-8-New-Features-Examples/src/java8_programs/Find_First_Non_Repeating_Character_In_String_Using_StreamAPI.java
@@ -5,17 +5,32 @@ import java.util.LinkedHashMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class Find_First_Non_Repeating_Character_In_String_Using_StreamAPI {
+
+public class First_Non_Repeating_Character_In_String_Using_Java8 {
 
 	public static void main(String[] args) {
-		String sentence = "LenovoLaptop";
-
-		String firstNonRepeatingChar = Arrays.stream(sentence.split(""))
-				.collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
-				.entrySet().stream().filter(f -> f.getValue() == 1).findFirst().get().getKey();
-
-		System.out
-				.println("First Non-repeating character in the string " + sentence + " is \n" + firstNonRepeatingChar);
+		 String str = "aeroaero"; // Output :: First Non Repeating Character Not Found
+		 String str2 = "aeroplane"; // Output :: r
+		 Arrays.stream(str.trim().split(""))
+		 	.collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+		 	.entrySet()
+		 	.stream()
+		 	.filter(f -> f.getValue() == 1)
+		 	.map(x -> x.getKey())
+		 	.findFirst()
+		 	.ifPresentOrElse(System.out::println, () -> {
+		 		System.out.println("First Non Repeating Character Not Found");
+		 	});
 	}
-
+	
+	/* Explanation 
+	 * 
+	 * Here, LinkedHashMap is used, as we need to follow the order strictly.
+	 * 
+	 * Here, ifPresentOrElse takes 2 arguments.
+	 * 
+	 * If the value is present, it prints that value.
+	 * If the value is not present, it executes the provided Runnable function.
+	 * 
+	 * */
 }
